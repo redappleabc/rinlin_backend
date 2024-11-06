d65d13beb9f318fe6ee6816744de6a8314327954
honestyman <leel50296@gmail.com>
2024-11-06 05:30:16 -0800

    add appleLogin

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -445,6 +445,57 @@ exports.googleLogin = async (req, res) => {
   }
 }
 
+exports.appleLogin = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where:{
+        email:req.body.email
+      }
+    });
+    if(user){
+      const userId = user.id;
+      const payload = { userId };
+      const accessToken =  jwt.sign(payload, process.env.ACCESS_SECRET)
+      const refreshToken =  jwt.sign(payload, process.env.REFRESH_SECRET)
+      const result = {
+        id: userId,
+        gender: user.gender,
+        accessToken: accessToken,
+        refreshToken: refreshToken
+      }
+      res.status(200).json(result);
+    } else {
+      const newuser = await User.create({
+        email: req.body.email,
+        pointCount: 50,
+        phrase1: "こんにちは！\n共通する趣味や性格に惹かれて、メッセージを送ってみました。\n私はADREESに在住するAGE歳のNAMEです。\nメッセージお待ちしております。",
+        phrase2: "初めまして。\n私は$uniqueADREESに在住する$uniqueAGE歳の$uniqueNAMEです。\nお互いまずはゆっくりとメッセージを重ねて仲良くなりたいです。\nよろしくお願いいたします。",
+        phrase3: "初めまして。$NAMEと申します。\nプロフィールを拝見して、ぜひ一度メッセージをしたいと思いご連絡いたしました。\nよろしくお願いいたします。",
+        verifyed: false,
+        paied: false,
+        experience: true,
+        viewUsers:0
+      });
+      const userId = newuser.id
+      
+      const payload = { userId };
+      const accessToken =  jwt.sign(payload, process.env.ACCESS_SECRET)
+      const refreshToken =  jwt.sign(payload, process.env.REFRESH_SECRET)
+      const result = {
+        id: userId,
+        gender: newuser.gender,
+        accessToken: accessToken,
+        refreshToken: refreshToken
+      }
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || 'Failed in Login'
+    })
+  }
+}
+
 exports.lineLogin = async (req, res) => {
   try {
     const user = await User.findOne({
